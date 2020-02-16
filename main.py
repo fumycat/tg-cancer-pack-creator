b0f2a1818440b588647cb750f71115e385fd9027
Fumycat <fumycat@gmail.com>
2020-02-16 22:31:27 +0700

    rev test

--- main.py
+++ main.py
@@ -1,51 +1,41 @@
 import os
-import random
-import string
 import time
 import logging
 import shutil
 import threading
-from urllib.parse import urlparse
-import redis
-import uuid
-import telebot
+
+import logging
 from PIL import Image
-from bottle import route, run, template, static_file, request
+
+from aiogram import Bot, Dispatcher, executor, types
 
 from tools import box
 
+API_TOKEN = 'BOT TOKEN HERE'
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-bot = telebot.TeleBot(os.environ.get("TG"))
-owner = os.environ.get("OWNER")
-bot_username = 'my_tg_cancer_bot'
-url = urlparse(os.environ.get('REDISCLOUD_URL'))
-redi = redis.Redis(host=url.hostname, port=url.port, password=url.password)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
 
 for n in ['/tmp/input', '/tmp/sliced']:
     if not os.path.exists(n):
         os.makedirs(n)
 
-def randomString(stringLength=10):
-    """Generate a random string of fixed length """
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
-        
+
 def process(file_name, name=None, title=None, emojis='😀'):
     img = Image.open('/tmp/input/' + file_name + '.jpg').resize((2560,2560), Image.ANTIALIAS).convert('RGB')
     p = '/tmp/sliced/' + file_name
     os.makedirs(p)
     for k, v in box.items():
         img.crop(v).save(p + '/' + str(k) + '.png')
-    #
+
     if name == None:
         name = 's' + file_name + '_by_' + bot_username
     if title == None:
         title = 'Tg Cancer Pack Creator'
-    ##
+
     with open('/tmp/sliced/{}/1.png'.format(file_name), 'rb') as f:
         print(bot.create_new_sticker_set(user_id=owner, name=name, title=title, png_sticker=f, emojis=emojis, contains_masks=False))
     for x in range(2, 26):
@@ -58,47 +48,9 @@ def process(file_name, name=None, title=None, emojis='😀'):
     os.remove('/tmp/input/' + file_name + '.jpg')
     shutil.rmtree('/tmp/sliced/' + file_name)
 
-
-@route('/static/:path#.+#', name='static')
-def static(path):
-    return static_file(path, root='static')
-
-
-@route("/j/<i>/<x>")
-def req(i, x):
-    if str(x) == '0':
-        return template('templates/req.html')
-    else:
-        state = redi.get(i).decode('utf-8')
-        print(state)
-        if state.isdigit():
-            response = state
-        else:
-            response = 'Ваш пак готов: https://t.me/addstickers/' + state
-        return response
-    
-
-
-@route("/")
-def main():
-    return template('templates/index.html')
-
-
-@route('/upload', method='POST')
-def do_upload():
-    upload = request.files.get('picture')
-    r = uuid.uuid4().hex
-    upload.save('/tmp/input/' + r + '.jpg')
-
-
-    print(dict(name=r, ip=request.environ.get('REMOTE_ADDR'), md5='TODO'))
-
-    threading.Thread(target=process, args=[r]).start()
-    redi.set(r, '0'.encode('utf-8'))
-    print(os.listdir())
-    return r
-
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 if __name__ == '__main__':
-    # run(host='localhost', port=8080, reloader=True)
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    executor.start_polling(dp, skip_updates=True)
